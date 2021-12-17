0f4d5c9cc644b096dadf4368aa1fa7e3eac7afd4
mahfantuci <maria@imaginationmedia.com>
2021-12-17 13:38:58 -0300

    Removido a implementação do código do sidebar para o component próprio

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+import { api } from "../services/api";
+import { useEffect, useState } from "react";
+
+import "./styles/sidebar.scss";
+
+interface SideBarProps {
+  selectedGenreId: Number;
+  OnHandleClickButton: (id: number) => void;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+export function SideBar({
+  selectedGenreId,
+  OnHandleClickButton,
+}: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => OnHandleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
