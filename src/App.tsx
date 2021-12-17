8a3aa9bc523eeca924f26325fc532c1a87b22cdc
mahfantuci <maria@imaginationmedia.com>
2021-12-17 13:46:36 -0300

    Movido o código do content para o componente próprio

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
+import { SideBar } from "./components/SideBar";
+import { Content } from "./components/Content";
 
- import { SideBar } from './components/SideBar';
- import { Content } from './components/Content';
-
-
-
-import './styles/global.scss';
-
-
-import './styles/content.scss';
-
-
-
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
+import "./styles/global.scss";
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
-  const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
-  
-
-  useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
-      setMovies(response.data);
-    });
-
-    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
-      setSelectedGenre(response.data);
-    })
-  }, [selectedGenreId]);
 
   function onHandleClickButton(id: number) {
     setSelectedGenreId(id);
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <SideBar selectedGenreId={selectedGenreId} OnHandleClickButton={onHandleClickButton}>
-
-      </SideBar>
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <SideBar
+        selectedGenreId={selectedGenreId}
+        OnHandleClickButton={onHandleClickButton}
+      />
+      <Content selectedGenreId={selectedGenreId} />
     </div>
-  )
+  );
 }
